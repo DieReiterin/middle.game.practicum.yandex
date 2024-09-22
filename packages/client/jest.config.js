69cfa97a0ba456bbc5441016d0b6f2fb654ba02b
rmkurbonov <rmkurbonov@sberbank.ru>
2024-09-22 15:38:07 +0300

    SCRUM-20_registr-from fix tests

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
+console.log('<rootDir>/config/jest/cssTransform.js')
 
 export default {
   preset: 'ts-jest',
@@ -7,5 +8,8 @@ export default {
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/config/jest/cssTransform.js',
   },
 }
