7af2532cf70019dc04782ae4bd258f600cf4e691
rmkurbonov <rmkurbonov@sberbank.ru>
2024-09-22 16:05:08 +0300

    SCRUM-20_registr-form remove required

--- packages/client/src/pages/Registration/Registration.tsx
+++ packages/client/src/pages/Registration/Registration.tsx
@@ -8,13 +8,13 @@ export const Registration: FC = () => {
       title="Регистрация"
       name="registration"
       items={[
-        { label: 'Почта', name: 'email', required: true },
-        { label: 'Логин', name: 'login', required: true },
-        { label: 'Имя', name: 'first_name', required: true },
-        { label: 'Фамилия', name: 'second_name', required: true },
-        { label: 'Телефон', name: 'phone', required: true },
-        { label: 'Пароль', name: 'password', required: true },
-        { label: 'Пароль (ещё раз)', name: 'password_ok', required: true },
+        { label: 'Почта', name: 'email' },
+        { label: 'Логин', name: 'login' },
+        { label: 'Имя', name: 'first_name' },
+        { label: 'Фамилия', name: 'second_name' },
+        { label: 'Телефон', name: 'phone' },
+        { label: 'Пароль', name: 'password' },
+        { label: 'Пароль (ещё раз)', name: 'password_ok' },
       ]}
       buttons={[
         {
