0f2152d27b0dd6814106671b291dbf70f4b85adb
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-09-25 15:51:27 +0200

    SCRUM-8 feat(vite): add alias for styles folder in config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import path from 'path'
 dotenv.config()
 
 // https://vitejs.dev/config/
@@ -19,4 +20,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/assets/styles'),
+    },
+  },
 })
