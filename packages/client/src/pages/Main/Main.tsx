1ceb4bb7c83150a7603e1b907255f62e3bcdb781
Sergej Bopp <58397321+lunarkbot@users.noreply.github.com>
2024-09-25 15:50:46 +0200

    SCRUM-8 refactor(Main): extract buttons into a separate variable

--- packages/client/src/pages/Main/Main.tsx
+++ packages/client/src/pages/Main/Main.tsx
@@ -1,8 +1,15 @@
 import { Box } from '@mui/material'
-import { Navigation } from './conponents'
+import { Navigation, NavigationProps } from './conponents'
 import { FC } from 'react'
 import styles from './Main.module.scss'
 import logo from '../../assets/images/logo.png'
+
+const buttons: NavigationProps['buttons'] = [
+  { title: 'Начать игру', link: '/game' },
+  { title: 'Профиль и Настройки', link: '/profile' },
+  { title: 'Лидерборд', link: '/leaderboard' },
+  { title: 'Форум', link: '/forum' },
+]
 
 export const Main: FC = () => {
   return (
@@ -11,26 +18,7 @@ export const Main: FC = () => {
         <Box className={styles.logoWrapper}>
           <img src={logo} className={styles.logo} alt="Mage Fight Logo" />
         </Box>
-        <Navigation
-          buttons={[
-            {
-              title: 'Начать игру',
-              link: '/game',
-            },
-            {
-              title: 'Профиль и Настройки',
-              link: '/profile',
-            },
-            {
-              title: 'Лидерборд',
-              link: '/leaderboard',
-            },
-            {
-              title: 'Форум',
-              link: '/forum',
-            },
-          ]}
-        />
+        <Navigation buttons={buttons} />
       </Box>
     </Box>
   )
