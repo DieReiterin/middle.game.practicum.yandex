7ca2cae34e8e2eaa65adf9af29d8abd179573f56
Margo <m.goncharova@group-ib.com>
2024-09-28 22:48:37 +0300

    SCRUM-22 review + merge fixes

--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -26,52 +26,54 @@ export const Leaderboard: FC = () => {
   )
 
   return (
-    <Box className={styles.main}>
-      <Typography variant="h5" align="center" sx={{ marginBottom: '16px' }}>
-        Лидерборд
-      </Typography>
-      <TableContainer
-        sx={{ color: 'text.secondary' }}
-        className={styles.tableContainer}>
-        <Table stickyHeader className={styles.table}>
-          <TableHead className={styles.tableHead}>
-            <TableRow>
-              <TableCell>Место</TableCell>
-              <TableCell>Игрок</TableCell>
-              <TableCell>Очки</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody className={styles.tableBody}>
-            {rows.map(row => (
-              <TableRow
-                key={`leaderboard-${row.name}`}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell scope="row">
-                  <Box className={styles.place}>{row.place}</Box>
-                </TableCell>
-                <TableCell>
-                  <Box className={styles.nameRow}>
-                    <Box
-                      component="img"
-                      className={styles.image}
-                      src={row.avatar ?? NoImage}
-                    />
-                    {row.name}
-                  </Box>
-                </TableCell>
-                <TableCell>{row.points} очков</TableCell>
+    <Box className={styles.wrapper}>
+      <Box className={styles.main}>
+        <Typography variant="h5" align="center" sx={{ marginBottom: '16px' }}>
+          Лидерборд
+        </Typography>
+        <TableContainer
+          sx={{ color: 'text.secondary' }}
+          className={styles.tableContainer}>
+          <Table stickyHeader className={styles.table}>
+            <TableHead className={styles.tableHead}>
+              <TableRow>
+                <TableCell>Место</TableCell>
+                <TableCell>Игрок</TableCell>
+                <TableCell>Очки</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <ButtonLink
-        sx={{ marginTop: 'auto', width: '100px' }}
-        disableElevation={true}
-        variant="contained"
-        to={PathsRoutes.Main}>
-        Назад
-      </ButtonLink>
+            </TableHead>
+            <TableBody className={styles.tableBody}>
+              {rows.map(row => (
+                <TableRow
+                  key={`leaderboard-${row.name}`}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell scope="row">
+                    <Box className={styles.place}>{row.place}</Box>
+                  </TableCell>
+                  <TableCell>
+                    <Box className={styles.nameRow}>
+                      <Box
+                        component="img"
+                        className={styles.image}
+                        src={row.avatar ?? NoImage}
+                      />
+                      {row.name}
+                    </Box>
+                  </TableCell>
+                  <TableCell>{row.points} очков</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <ButtonLink
+          sx={{ marginTop: 'auto', width: '100px' }}
+          disableElevation={true}
+          variant="contained"
+          to={PathsRoutes.Main}>
+          Назад
+        </ButtonLink>
+      </Box>
     </Box>
   )
 }
