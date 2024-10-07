0365fb7ae6c183564a3e6d7ab37a3af692d5cda2
Margo <m.goncharova@group-ib.com>
2024-10-07 16:46:36 +0300

    SCRUM-42 service worker has been added with another strategy

--- packages/client/sw.js
+++ packages/client/sw.js
@@ -15,32 +15,44 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('fetch', event => {
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        const fetchRequest = event.request.clone()
+
         if (response) {
-          return response;
+          void update(fetchRequest)
+          return response
         }
 
-        const fetchRequest = event.request.clone();
-        return fetch(fetchRequest)
-          .then(response => {
-              if(!response || response.status !== 200 || response.type !== 'basic') {
-                return response;
-              }
-
-              const responseToCache = response.clone();
-              caches.open(CACHE_NAME)
-                .then(cache => {
-                 void cache.put(event.request, responseToCache);
-                });
-              return response;
-            }
-          );
+        return update(fetchRequest)
       })
-  );
-});
+  )
+})
 
+
+
+const update = (request) => {
+  return fetch(request)
+    .then(response => {
+        if (!response || response.status !== 200) {
+          return response
+        }
+        const responseToCache = response.clone()
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            void cache.put(request, responseToCache)
+          })
+
+        return response
+      }
+    ).catch(err => {
+    return err
+  })
+
+}
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
