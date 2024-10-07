c7dea2366b3db917e7ebf9ae966b7733cb0df87f
Serafim Vasilev <zzerafim@ya.ru>
2024-10-07 19:16:39 +0300

    `SCRUM-40` minor cleanup

--- packages/client/src/router/index.tsx
+++ packages/client/src/router/index.tsx
@@ -1,54 +1,49 @@
 import { Navigate, RouteObject } from 'react-router-dom'
 import {
-  Login,
-  Registration,
-  Main,
-  GamePage,
-  Leaderboard,
-  Error,
+    Login,
+    Registration,
+    Main,
+    GamePage,
+    Leaderboard,
+    Error,
 } from '../pages'
-import GameDemo from '../game/GameDemo'
 import { PathsRoutes } from './types'
 
 const closePathUnauthorized = (
-  isAuthorized: boolean,
-  page: JSX.Element
+    isAuthorized: boolean,
+    page: JSX.Element
 ): JSX.Element =>
-  isAuthorized ? page : <Navigate to={PathsRoutes.Login} replace />
+    isAuthorized ? page : <Navigate to={PathsRoutes.Login} replace />
 
 const closePathAuthorized = (
-  isAuthorized: boolean,
-  page: JSX.Element
+    isAuthorized: boolean,
+    page: JSX.Element
 ): JSX.Element =>
-  isAuthorized ? <Navigate to={PathsRoutes.Main} replace /> : page
+    isAuthorized ? <Navigate to={PathsRoutes.Main} replace /> : page
 
 export const getRoutes = (isAuthorized: boolean): RouteObject[] => [
-  {
-    path: PathsRoutes.Registration,
-    element: closePathAuthorized(isAuthorized, <Registration />),
-  },
-  {
-    path: PathsRoutes.Login,
-    element: closePathAuthorized(isAuthorized, <Login />),
-  },
-  {
-    path: PathsRoutes.Main,
-    element: closePathUnauthorized(isAuthorized, <Main />),
-  },
-  {
-    path: PathsRoutes.GamePage,
-    element: closePathUnauthorized(isAuthorized, <GamePage />),
-  },
-  {
-    path: '*',
-    element: <Error title="404" text="Не туда попали" />,
-  },
-  {
-    path: PathsRoutes.Leaderboard,
-    element: <Leaderboard />,
-  },
-  {
-    path: PathsRoutes.GameDemo,
-    element: <GameDemo />,
-  },
+    {
+        path: PathsRoutes.Registration,
+        element: closePathAuthorized(isAuthorized, <Registration />),
+    },
+    {
+        path: PathsRoutes.Login,
+        element: closePathAuthorized(isAuthorized, <Login />),
+    },
+    {
+        path: PathsRoutes.Main,
+        element: closePathUnauthorized(isAuthorized, <Main />),
+    },
+    {
+        path: PathsRoutes.GamePage,
+        element: closePathUnauthorized(isAuthorized, <GamePage />),
+    },
+    {
+        path: '*',
+        element: <Error title="404" text="Не туда попали" />,
+    },
+    {
+        path: PathsRoutes.Leaderboard,
+        element: <Leaderboard />,
+    },
 ]
